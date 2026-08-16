de4edbc402241f0d86e781bf4a7b8e2513651977
laurence <laurence@Ubuntu18.myguest.virtualbox.org>
2026-08-16 21:28:49 +0800

    Created task scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_08_16_121734) do
+ActiveRecord::Schema[8.1].define(version: 2026_08_16_125316) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", null: false
+    t.string "title"
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.text "description"
+    t.boolean "done"
     t.string "title"
     t.datetime "updated_at", null: false
   end
